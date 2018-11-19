6767cac320663ea06cd0be1260006e18785842a5
Aretha Walls <admin@arethawalls.com>
2018-11-19 10:39:04 -0700

    Added additional text under images & toggle animation

--- assets/app.js
+++ assets/app.js
@@ -3,8 +3,6 @@ $(document).ready(function () {
     var topics = ['noodles', 'taco', 'burger', 'sushi', 'fondue', 'chocolate', 'steak',
         'ice cream', 'bagel', 'dumplings']
     var key = 'YO1V30OsrDmHaofembwPUeRHKrS1WYDB';
-    // var queryUrl = 'https://api.giphy.com/v1/gifs/search?api_key=' + key + '&q=' + topic + '&limit=10';
-
 
     for (let i = 0; i < topics.length; i++) {
         //For each topic, create a button, give it the 'gif-button' class, and set the data-topic attribute:
@@ -19,15 +17,34 @@ $(document).ready(function () {
             method: 'GET'
         }).then(function(response) {
             $('.gif-box').empty();
-            var $gifDiv = $('<div>').addClass('gif-div');
+            console.log(response)
             for(let i = 0; i < response.data.length; i++){
+                let thisGif = response.data[i];
+                var $gifDiv = $('<div>').addClass('gif-div');
                 $gifDiv.append($('<img>').attr({
-                    'src' : response.data[i].images.original_still.url,
-                    'data-animate-src' : response.data[i].images.original.url
+                    'class' : 'gif',
+                    'src' : thisGif.images.original_still.url,
+                    'data-still-src' : thisGif.images.original_still.url,
+                    'data-animate-src' : thisGif.images.original.url
                 }));
+                $gifDiv.append(
+                    '<h3>' + response.data[i].title + '</h3>' + 
+                    '<p><strong>Rating:</strong> ' + thisGif.rating.toUpperCase() + '</p>'
+                );
+                if(thisGif.hasOwnProperty('user')){
+                    $gifDiv.append('<p><strong>Creator:</strong> <a href="' + thisGif.user.profile_url + '" target="_blank">' + thisGif.user.username + '</a></p>');
+                }
+                $gifDiv.appendTo($('.gif-box'))
             }
-            $gifDiv.appendTo($('.gif-box'))
         });
+    });
+
+    $('.gif-box').on('click', '.gif', function() {
+        if($(this).attr('src') === $(this).attr('data-still-src')) {
+            $(this).attr('src', $(this).attr('data-animate-src'));
+        } else {
+            $(this).attr('src', $(this).attr('data-still-src'));
+        }
     });
 
 });
