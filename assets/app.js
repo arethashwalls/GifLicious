efa0650f82c54078e5440363c2fd3edaa5f3b218
Aretha Walls <admin@arethawalls.com>
2018-11-19 10:14:19 -0700

    Can display 10 gifs for each topic

--- assets/app.js
+++ assets/app.js
@@ -1,27 +1,33 @@
-var key = 'YO1V30OsrDmHaofembwPUeRHKrS1WYDB';
-var topic = 'cat';
-var queryUrl = 'https://api.giphy.com/v1/gifs/search?api_key=' + key + '&q=' + topic + '&limit=10';
+$(document).ready(function () {
 
-var display = document.getElementById('display');
+    var topics = ['noodles', 'taco', 'burger', 'sushi', 'fondue', 'chocolate', 'steak',
+        'ice cream', 'bagel', 'dumplings']
+    var key = 'YO1V30OsrDmHaofembwPUeRHKrS1WYDB';
+    // var queryUrl = 'https://api.giphy.com/v1/gifs/search?api_key=' + key + '&q=' + topic + '&limit=10';
 
 
-var xhr = new XMLHttpRequest();
-
-xhr.onload = function () {
-    if (xhr.status >= 200 && xhr.status < 300) {
-        console.log('success!', xhr);
-        var source = JSON.parse(xhr.response).data[0].images.original_still.url;
-        display.innerHTML = '<img src="' + source + '" />';
-        console.log(source);
-    } else {
-        console.log('The request failed!');
+    for (let i = 0; i < topics.length; i++) {
+        //For each topic, create a button, give it the 'gif-button' class, and set the data-topic attribute:
+        var $newButton = $('<button>').text(topics[i]).addClass('gif-button').attr('data-topic', topics[i]);
+        // $('.button-box').append($newButton);
+        $newButton.appendTo($('.button-box'))
     }
-}
 
-xhr.open('GET', queryUrl);
-xhr.send();
+    $('.button-box').on('click', '.gif-button', function() {
+        $.ajax({
+            url: 'https://api.giphy.com/v1/gifs/search?api_key=' + key + '&q=' + $(this).attr('data-topic') + '&limit=10',
+            method: 'GET'
+        }).then(function(response) {
+            $('.gif-box').empty();
+            var $gifDiv = $('<div>').addClass('gif-div');
+            for(let i = 0; i < response.data.length; i++){
+                $gifDiv.append($('<img>').attr({
+                    'src' : response.data[i].images.original_still.url,
+                    'data-animate-src' : response.data[i].images.original.url
+                }));
+            }
+            $gifDiv.appendTo($('.gif-box'))
+        });
+    });
 
-
-
-
-//document.getElementById('display').innerHTML('<img src=' + '>')
+});
